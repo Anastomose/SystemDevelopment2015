a5c31070da212ca1517d4b439d816012a4ff6d89
Anastomose <anastomose@yahoo.com>
2015-04-07 20:36:49 -0700

    class excercises

--- Examples/week-01-testing/calculator/calculator_test.py
+++ Examples/week-01-testing/calculator/calculator_test.py
@@ -11,6 +11,12 @@ class TestCalculatorFunctions(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(self.x, self.y), 5)
 
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(self.y, self.x), 1)
+
+    def test_floater(self):
+        self.assertEqual(calc.add(0.5, 0.5), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
--- Examples/week-01-testing/context_manager/exercise1.py
+++ Examples/week-01-testing/context_manager/exercise1.py
@@ -1,4 +1,17 @@
-with YourExceptionHandler():
+import traceback as tb
+
+class myexceptionhandler(object):
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, type, value, traceback):
+        print tb.print_tb(traceback)
+        return True # tear it down
+
+with myexceptionhandler():
     print "do some stuff here"
     1/0
 
